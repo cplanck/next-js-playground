832d7a6934f4d14e49330958742448df729f749b
Cameron Planck <cameronplanck@gmail.com>
2023-01-13 20:49:26 -0500

    third commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,7 +40,6 @@ export default function Home() {
             </a>
           </div>
         </div>
-
         <div className={styles.center}>
           <Image
             className={styles.logo}
