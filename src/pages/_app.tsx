0b53da8f6111ed26e135da1aa9fc68715f4ff9ef
Cameron Planck <cameronplanck@gmail.com>
2023-01-15 12:38:59 -0500

    fourth commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -43,7 +43,7 @@ export default function App({ Component, pageProps }: AppProps) {
         {/* <CssBaseline /> */}
         {/* <SwipeableTemporaryDrawer /> */}
         <Example/>
-        <Component {...pageProps}/>
+        {/* <Component {...pageProps}/> */}
       {/* </ThemeProvider> */}
     </QueryClientProvider>   
   )
