65e8aa58a005785e59547aac9d67b53328069894
Cameron Planck <cameronplanck@gmail.com>
2023-01-15 09:19:54 -0500

    fourth commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,40 @@
-import '@/styles/globals.css'
+// import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import SwipeableTemporaryDrawer from '../components/Navbar'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 const queryClient = new QueryClient()
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
     <QueryClientProvider client={queryClient}>  
-      <>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
         <SwipeableTemporaryDrawer />
-        <Component {...pageProps} />
-      </>
+        <Component {...pageProps}/>
+      </ThemeProvider>
     </QueryClientProvider>   
   )
 }
--- src/pages/datapage.tsx
+++ src/pages/datapage.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+// import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
 import BasicCard from '../components/Card'
 import TemporaryDrawer from '../components/SideDrawer'
@@ -20,7 +20,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main>
        <BasicCard />
       </main>
     </>
